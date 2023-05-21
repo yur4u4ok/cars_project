1c7efdad39825f820d890edaf847f5ae636b0010
Ostap Yurchuk <ostap4uk43yurchuk@gmail.com>
2023-05-21 15:08:41 +0300

    done

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["myappydjango.us-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 AUTH_USER_MODEL = 'users.UserModel'
